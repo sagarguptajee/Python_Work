ca8b784cd709128d9a1139b821e0d55e49b2d1c1
Sagar <sagarguptajee@gmail.com>
2025-12-23 10:15:55 +0530

    all

---
+++ ClassWork/datatype.py
@@ -0,0 +1,41 @@
+# a =10
+# print(type(a))
+
+# input function
+a=int (input("enter your number:"))
+print(type(a))
+
+
+# Data type string
+
+name="sagar"
+print(name)
+print(type(name))
+
+# list
+l=[10,20.30,40,50]
+print(l)
+print(type(l))
+
+# tuple
+
+t=(10,20.30,40,50)
+print(t)
+print(type(t))
+
+
+# dictonary
+
+st= {"name":"sagar","sagar@gmail.com":"email"}
+print(st)
+print(type(st))
+
+# Set
+s={10,20,30,40,50,60,60}
+print(s)
+print(type(s))
+
+# boolean
+
+b=True
+print(b)
---
+++ ClassWork/operators.py
@@ -0,0 +1,88 @@
+# arithmatic 
+
+a=10
+b=20
+print(a+b)
+print("abc"+"xyx")
+print("abc"+str(a))
+print(a-b)
+print(a*b)
+print(a/b)
+
+# floor
+print(10//3)
+
+print(10**3)
+
+# asigmant operator
+# a=10
+
+# a+=10
+# a-=10
+# a*=2
+# a/=2
+# a//=3
+# a**=2
+
+# comparison or relation
+# a=10
+# b=20
+# c=10
+# print(a==b)
+# print(a==c)
+# print(a!=c)
+# print(a>b)
+# print(a>=c)
+
+
+# Logical  and or not
+
+print(True and False)
+print(True and True)
+print(False and True)
+print(False and False)
+
+print("***************" )
+
+print(True or False)
+print(True or True)
+print(False or True)
+print(False or False)
+
+print("************************")
+
+print(not True)
+
+
+
+# identity
+# is , not is
+
+
+a=[10,20]
+b=[10,20]
+
+print(a==b)
+print(a is b)
+print(a is not b)
+
+
+a=1000
+b=1000
+
+print(type(a))
+print(a==b)
+print(a is b)
+
+
+s="abc"
+s1=s.capitalize()
+print(s1)
+
+# membership
+
+a=[10,20,30]
+b="abc"
+print(10 in a)
+print(100 not in a)
+print('abcc' in b)
