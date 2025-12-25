1dff0aacd426201e533e511c66a15db491355404
Sagar <sagarguptajee@gmail.com>
2025-12-25 09:48:04 +0530

    all

--- ClassWork/test.py
+++ ClassWork/test.py
@@ -1,1 +1,16 @@
-print("hello")
+
+marks=int (input("enter your marks:"))
+
+if marks>=91 and marks<=100:
+    print("Your gtrade is A") 
+elif marks>=71 and marks<=90:
+    print("Your gtrade is B")
+elif marks>=51 and marks<=70:
+    print("Your gtrade is C")
+elif marks>=35 and marks<=50:
+    print("Your gtrade is D")    
+elif marks==0 and marks<=34:
+    print("You Are Failed")
+else:
+    print("Please type Inavlid Marks")
+
