9eef65e634af3fc502db610afd38ef3f50075498
Sagar <sagarguptajee@gmail.com>
2025-12-30 09:32:19 +0530

    gg

--- ClassWork/conditional_Task.py
+++ ClassWork/conditional_Task.py
@@ -1,13 +1,14 @@
-# num=int(input("enter your number for table:"))
-# for i in range(1,11):
-#     tab=num*i
-#     print(tab)
+num=int(input("enter your number for table:"))
+for i in range(1,11):
+    tab=num*i
+    print(f"{num} * {i} = {tab}")
+    # print(tab)
 
 
-for i in range(1,101):
-    if i%2==0:
-        print(f"{i} is Even Number")
-    else:
-        print(f"{i} is odd number")  
+# for i in range(1,101):
+#     if i%2==0:
+#         print(f"{i} is Even Number")
+#     else:
+#         print(f"{i} is odd number")  
 
 
